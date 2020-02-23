d5ab654a2dd7cafb9a8be2012cfefc7d94feaaf8
Joonas <joonas.heinonen@edu.turkuamk.fi>
2020-02-23 13:38:51 +0200

    Added libraries and simple visualization.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BarChart from './BarChart'
+
+const d3 = require('d3');
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>D3Viz</h2>
+      <div>
+        <BarChart data={[10, 6, 8]} size={[500, 500]} />
+      </div>
     </div>
   );
 }
